f46eaa25d0129416c5c45654a0e640f04d57ea56
krishan041 <krishandara04@gmail.com>
2025-01-25 19:02:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "b84d7d4dfa33904d36b85e1ead16bd63")
     AUTH_USER = os.environ.get('AUTH_USERS', "6428531614").split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
-    HOST = "https://drm-api-six.vercel.app"
+    # Proxy settings (if required)
+    HTTP_PROXY = os.environ.get("HTTP_PROXY", "").strip()
     CREDIT = " 𝗝𝗮𝗮𝘁™"#Here You Can Change with Your Name  or any custom name or title you prefer
