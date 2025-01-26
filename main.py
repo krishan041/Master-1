d57e901e29ea87b4f24ea53b434167b15daf84b5
krishan041 <krishandara04@gmail.com>
2025-01-26 10:08:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from config import Config
 from pyrogram import Client as bot, idle
 import asyncio
 import logging
+from flask import Flask
+
+app = Flask(__name__)
+app.config.from_object(Config)
 
 logging.basicConfig(
     level=logging.INFO,    
